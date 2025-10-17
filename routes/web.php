a8bce7ca332142818c033f5023cc0cdba88e6f31
raziy <raziy.ibrahim@gmail.com>
2025-10-17 14:35:25 +0700

    raziy

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,39 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
+// Halaman awal mengarahkan ke halaman login
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
-Route::get('/tentang-kami', function () {
- return view('about'); // Kita akan menampilkan view bernama 'about'
+// Grup untuk semua rute yang memerlukan otentikasi (login)
+Route::middleware('auth')->group(function () {
+    
+    // Rute Dashboard (memerlukan auth & verified)
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware('verified')->name('dashboard'); // 'auth' sudah ada di grup
+
+    // Rute Profil
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    // Rute Posts (CRUD)
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+    
+    // Saran: Gunakan Route::resource() untuk menyederhanakan rute CRUD posts
+    // Route::resource('posts', PostController::class)->except(['index', 'show']);
 });
 
-Route::get('/kontak', function () {
- return view('kontak'); // Kita akan menampilkan view bernama 'about'
-});
+
+require __DIR__.'/auth.php';
+
